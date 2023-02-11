2c33777af2410960528094e18bd111720990dce5
edijajuli <edijajuli1@gmail.com>
2023-02-11 20:10:41 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h2>hello world</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, temporibus.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
